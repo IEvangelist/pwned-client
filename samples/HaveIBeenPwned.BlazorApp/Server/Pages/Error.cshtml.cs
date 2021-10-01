2f4103961f3f33efe3667de542ee34f91756bcae
David Pine <david.pine@microsoft.com>
2021-09-30 21:20:03 -0500

    Fix build errors

--- samples/HaveIBeenPwned.BlazorApp/Server/Pages/Error.cshtml.cs
+++ samples/HaveIBeenPwned.BlazorApp/Server/Pages/Error.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
 namespace HaveIBeenPwned.BlazorApp.Server.Pages;
+
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
