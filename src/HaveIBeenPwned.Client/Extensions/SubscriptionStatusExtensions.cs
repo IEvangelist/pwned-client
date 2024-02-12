4f8444e427527d09048da2e7817c86a3715771c6
David Pine <david.pine.7@gmail.com>
2024-02-12 06:27:13 -0600

    Added ext

---
+++ src/HaveIBeenPwned.Client/Extensions/SubscriptionStatusExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) David Pine. All rights reserved.
+// Licensed under the MIT License.
+
+namespace HaveIBeenPwned.Client.Extensions;
+
+/// <summary>
+/// Extensions methods for the <see cref="SubscriptionStatus"/> class.
+/// </summary>
+public static class SubscriptionStatusExtensions
+{
+    /// <summary>
+    /// Gets the <see cref="HibpSubscriptionLevel"/> from the given <paramref name="subscriptionStatus"/>.
+    /// </summary>
+    /// <param name="subscriptionStatus">The subscription status instance to evaluate.</param>
+    /// <returns>The corresponding subscription level.</returns>
+    /// <exception cref="ArgumentException">
+    /// Thrown if the <see cref="SubscriptionStatus.SubscriptionName"/>
+    /// doesn't map to a level.
+    /// </exception>
+    public static HibpSubscriptionLevel GetSubscriptionLevel(
+        this SubscriptionStatus subscriptionStatus)
+    {
+        return subscriptionStatus.SubscriptionName switch
+        {
+            "Pwned 1" => HibpSubscriptionLevel.One,
+            "Pwned 2" => HibpSubscriptionLevel.Two,
+            "Pwned 3" => HibpSubscriptionLevel.Three,
+            "Pwned 4" => HibpSubscriptionLevel.Four,
+
+            _ => throw new ArgumentException(
+                $"Unknown subscription level: {subscriptionStatus.SubscriptionName}.")
+        };
+    }
+}
